f03cdc5eee3d977833a59e45274bdea755a7cc2f
Danny JeanLouis <dlouis3386@gmail.com>
2021-04-25 14:15:01 -0400

    finished script for more / less toggle

--- scripts/app.js
+++ scripts/app.js
@@ -2,14 +2,38 @@ const toggleBtn = document.querySelector(".toggle");
 const slideMenu = document.querySelector(".slide-up");
 const quote = document.querySelector(".quote");
 const timer = document.querySelector(".timer");
+const toggleArrow = document.querySelector(".toggle-arrow");
+const toggleText = document.querySelector(".toggle-text");
 
-const toggleSlide = () => {
-  slideMenu.classList.toggle("toggleSlide");
-  quote.classList.toggle("quote-animation");
+const showMore = () => {
+  slideMenu.classList.add("toggleSlide");
+  quote.classList.remove("showquote");
+  quote.classList.add("hidequote");
+  toggleArrow.classList.add("rotate");
+  toggleText.textContent = "Less";
 
   setTimeout(function () {
     quote.style.display = "none";
   }, 1000);
 };
 
-toggleBtn.addEventListener("click", toggleSlide);
+const showLess = () => {
+  quote.style.display = "flex";
+
+  slideMenu.classList.remove("toggleSlide");
+  quote.classList.remove("hidequote");
+  quote.classList.add("showquote");
+  toggleArrow.classList.remove("rotate");
+  toggleText.textContent = "more";
+};
+
+const toggler = () => {
+  if (toggleText.textContent === "more") {
+    showMore();
+  } else {
+    showLess();
+    console.log("Create slide down function");
+  }
+};
+
+toggleBtn.addEventListener("click", toggler);
